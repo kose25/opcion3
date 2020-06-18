41390db4d9afd54f3d2153fd11949bf283f2e394
kose25 <carlosjosepg@ufps.edu.co>
2020-06-17 21:24:26 -0500

    corregido nombre de paquetes

--- src/main/java/com/opcion/demo/repository/UsuarioRepository.java
+++ src/main/java/com/opcion/demo/repository/UsuarioRepository.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.opcion.repository;
+package com.opcion.demo.repository;
 
 /**
  *
--- src/main/java/com/opcion3/demo/entites/Usuario.java
+++ src/main/java/com/opcion3/demo/entites/Usuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.opcion3.entites;
+package com.opcion3.demo.entites;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
